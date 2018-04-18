50e6a2201348becd6f23182e550fb576c10dc34f
Usman Khwaja <u.khwaja@gmail.com>
2018-04-18 20:25:32 +0500

    surah dropdown

--- src/components/myMuqri.vue
+++ src/components/myMuqri.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="main">
-    <h2>{{verses.name}}</h2>
-    <div class="text">
-    <p v-if="verses" class="verse"><strong class>{{verses.verse["verse_" + verseNumber]}}</strong></p>
-    <p v-if="translation" lang="en" class="translation"><strong class>{{translation.verse["verse_" + verseNumber]}}</strong></p>
-    </div>
-    <ul v-if="errors && errors.length">
-      <li v-for="error of errors" :key='error'>
-        {{error.message}}
-      </li>
-    </ul>
-    <div class="inputs">
-      <label for="surah_input">Surah</label>
-      <input id="surah_input" v-model="surahNumber" type="number" min="1" max="114" length="3" value="1">
-      <label for="verse_input">Verse</label>
-      <input id="verse_input" v-model="verseNumber" type="number" min="1" v-bind:max="verses.count" length="3" value="1">
+  <div class="main ui grid">
+    <div class="one column">
+      <h2>{{verses.name}}</h2>
+      <form class="form">
+        <div class="ui labeled input">
+          <label for="surah_input" class="ui label">Surah</label>
+          <select id="surah_input" class="ui dropdown" v-model="surahNumber" type="number" min="1" max="114" length="3" value="1">
+            <option v-for="(surah, index) in surahs" :key="index" :value="index + 1">{{surah.index}} - {{surah.title}}</option>
+          </select>
+        </div>
+        <div class="ui labeled input">
+          <label for="verse_input" class="ui label">Verse</label>
+          <input id="verse_input" v-model="verseNumber" type="number" min="1" v-bind:max="verses.count" length="3" value="1">
+        </div>
+      </form>
+      <div class="text">
+      <p v-if="verses" class="verse"><strong class>{{verses.verse["verse_" + verseNumber]}}</strong></p>
+      <p v-if="translation" lang="en" class="translation"><strong class>{{translation.verse["verse_" + verseNumber]}}</strong></p>
+      </div>
+      <ul v-if="errors && errors.length">
+        <li v-for="error of errors" :key='error'>
+          {{error.message}}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -28,6 +36,7 @@ export default {
   name: 'myMuqri',
   data () {
     return {
+      surahs: [],
       verses: [],
       translation: [],
       errors: [],
@@ -37,6 +46,14 @@ export default {
   },
 
   created () {
+    axios.get('../static/resources/surah.json')
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.surahs = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
     axios.get('../static/resources/Surah/surah_1.json')
       .then(response => {
         // JSON responses are automatically parsed.
@@ -88,42 +105,17 @@ export default {
     }
   }
 
-  // computed: {
-  //   surahNumber: {
-  //     get: () => {
-  //       return this.surahNumber
-  //     },
-  //     set: (newVal) => {
-  //       console.log('../static/resources/Surah/surah_' + newVal + '.json')
-  //       axios.get('../static/resources/Surah/surah_' + newVal + '.json')
-  //         .then(response => {
-  //           // JSON responses are automatically parsed.
-  //           this.verses = response.data
-  //         })
-  //     }
-  //   }
-  // }
-  // computed: {
-  //   // a computed getter
-  //   chosenVerseNumber: {
-  //     get: () => {
-  //       // `this` points to the vm instance
-  //       console.log(this.chosenVerseNumber)
-  //       return this.chosenVerseNumber
-  //     },
-  //     set: (newVal) => {
-  //       this.verseNumber = newVal
-  //       console.log(newVal)
-  //     }
-  //   }
-  // }
-
 }
 
+require('semantic-ui-css/semantic.css')
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.main {
+  width: 50%;
+  margin: 0 auto;
+}
 .verse {
   text-align: right;
   font-size: 22pt;
@@ -133,17 +125,17 @@ export default {
   text-align: left;
 }
 .text {
-  width: 50%;
-  display: block;
   margin: 0 auto;
-  width: 300px;
-  height: 300px;
-  background-color: blanchedalmond;
+  display: block;
+  max-width: 380px;
+  min-height: 300px;
+  background-color: rgb(169, 240, 208);
   padding: 2em;
   border-radius: 1em;
+  transition: all 3ms ease;
 }
-.inputs {
-  margin-top: 2em;
+.form {
+  margin: 2em auto;
 }
 h1, h2 {
   font-weight: normal;
