3cbd1975c08426a744214678362ddc28aa525e08
Usman Khwaja <u.khwaja@gmail.com>
2018-04-19 00:12:01 +0500

    jquery plugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -115,7 +115,11 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    new webpack.ProvidePlugin({
+      $ : 'jquery',
+      jQuery : 'jquery',
+    })
   ]
 })
 
