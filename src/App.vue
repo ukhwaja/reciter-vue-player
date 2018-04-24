29c2dac04bb9fac354057f66f9c2520d6aa91060
Usman Khwaja <u.khwaja@gmail.com>
2018-04-24 15:01:11 +0500

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <MyMuqri/>
+    <footer>
+      <div>Text JSON from <a href="https://github.com/semarketir/quranjson" target="_blank">github</a></div>
+      <div>Audio from <a href="http://everyayah.com/data/status.php" target="_blank">everyayah.com</a></div>
+      <div>Fonts from <a href="http://fonts.qurancomplex.gov.sa/" target="_blank">qurancomplex.gov.sa</a></div>
+      <div><i class="vuejs icon big" title="Vuejs"></i><a href="https://vuejs.org" target="_blank">Vue.js</a> project</div>
+      <div><i class="code icon"></i> with <i class="heart icon"></i> by <a href="https://numbfall.github.io" target="_blank">Usman</a></div>
+    </footer>
   </div>
 </template>
 
@@ -22,6 +29,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+}
+footer {
   margin-top: 60px;
 }
 </style>
